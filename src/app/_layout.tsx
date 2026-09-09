605abc9d92534c0845d6b096f2d7fa3881374b3a
MadeInRodri <rivasalexis1820@gmail.com>
2026-09-08 20:43:27 -0600

    Agregando navegación con Expo Router

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,38 @@
 import "../../global.css";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
+import { useEffect, useState } from "react";
+import { useAuthStore } from "@/store/authStore";
 
 export default function RootLayout() {
+  const router = useRouter();
+  const segments = useSegments();
+  const isLogged = useAuthStore((state) => state.isLogged);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setIsReady(true), 100);
+  }, []);
+
+  useEffect(() => {
+    if (!isReady) return;
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (!isLogged && !inAuthGroup) {
+      router.replace("/(auth)/login" as any);
+    } else if (isLogged && inAuthGroup) {
+      router.replace("/(tabs)" as any);
+    }
+  }, [isLogged, segments, isReady]);
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "#fff7ed" }, // bg-orange-50
+      }}
+    >
+      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(tabs)" />
     </Stack>
   );
 }
